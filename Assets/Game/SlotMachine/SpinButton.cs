8f5e92280562ab2140459f0ea3c09b487534049d
fatihturker <turkerfatih@gmail.com>
2025-08-27 01:25:04 +0300

    sound setup

---
+++ Assets/Game/SlotMachine/SpinButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game
+{
+    public class SpinButton:MonoBehaviour
+    {
+        private void Awake()
+        {
+            EventTrigger trigger = gameObject.AddComponent<EventTrigger>();
+            trigger.triggers.Add(new EventTrigger.Entry()
+            {
+                eventID = EventTriggerType.PointerClick,
+            });
+            trigger.triggers[0].callback.AddListener(Click);
+        }
+        
+        private void Click(BaseEventData eventData)
+        {
+            Services.SlotMachine.Spin();
+        }
+    }
+}
