9649d5f033abed5324588c17329eb597479ebe58
fatihturker <turkerfatih@gmail.com>
2025-09-03 16:29:33 +0300

    3d button

--- Assets/Game/SlotMachine/SpinButton.cs
+++ Assets/Game/SlotMachine/SpinButton.cs
@@ -1,15 +1,44 @@
 ﻿using System;
+using DG.Tweening;
 using Game.Actions;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Game
 {
-    public class SpinButton:MonoBehaviour
+    public class SpinButton:MonoBehaviour,IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler,IPointerDownHandler,IPointerUpHandler
     {
-        private void OnMouseUpAsButton()
+        Vector3 baseScale;
+
+        private void Awake()
+        {
+            baseScale = transform.localScale;
+        }
+        
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            transform.DOScale(baseScale * 1.1f,0.1f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.DOScale(baseScale,0.1f);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
         {
             Services.Actions.Add(new SpinAction());
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            transform.DOScale(baseScale*0.9f,0.1f).SetEase(Ease.OutSine);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            transform.DOScale(baseScale*1.1f,0.1f).SetEase(Ease.InBounce);
         }
     }
 }
