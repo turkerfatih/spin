c494a14bb89651143a981bab31d535011ab5d12b
fatihturker <turkerfatih@gmail.com>
2026-01-15 22:55:11 +0300

    updates on odometer

--- Assets/Game/Odometer/MeterController.cs
+++ Assets/Game/Odometer/MeterController.cs
@@ -18,7 +18,7 @@ namespace Game.Odometer
         [SerializeField] private float animationDuration = 0.4f; // Total time for all wheels to finish
         [SerializeField] private int delayBetweenTicksMs = 100;
 
-        private bool[] moving = { false, false, false };
+        private readonly bool[] moving = { false, false, false };
 
         void Start()
         {
@@ -33,7 +33,7 @@ namespace Game.Odometer
 
         private void InitializeDisplay(int value)
         {
-            int temp = value;
+            var temp = value;
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i].SetDigitInstant(temp % 10);
@@ -81,40 +81,6 @@ namespace Game.Odometer
             await parallelSequence.ToUniTask();
         }
         
-        private void UpdateAllNumbers(float totalValue)
-        {
-            float temp = totalValue;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                // Get the fractional part so the wheel "rolls" between numbers
-                float digitValue = temp % 10;
-                numbers[i].UpdateRotationContinuous(digitValue);
-                temp /= 10;
-            }
-        }
-        
-        public async UniTask DoFastJump(int targetValue, float duration)
-        {
-            // We create a temporary float to allow for smooth fractional rotation
-            float internalValue = currentValue;
-
-            await DOTween.To(() => internalValue, x => {
-                    internalValue = x;
-                    currentValue = Mathf.RoundToInt(x);
-                }, (float)targetValue, duration)
-                .SetEase(Ease.InOutQuart)
-                .OnUpdate(() => UpdateAllNumbers(internalValue))
-                .ToUniTask();
-            
-            // Final snap to ensure precision
-            UpdateAllNumbers(targetValue);
-        }
-        
-        [ContextMenu("Fast Countdown")]
-        public async void FastCountdown()
-        {
-            await DoFastJump(0, 2f);
-        }
         
         [ContextMenu("Mechanical Jump")]
         public async void MechanicalJump()
@@ -130,6 +96,7 @@ namespace Game.Odometer
             var change = currentValue - targetValue;
             Debug.Log($"{currentValue} - {change}={currentValue-change}");
             SubtractAndUpdateMoving(currentValue, change);
+            UpdateMovingForAnimation();
             Debug.Log($"{moving[2]} {moving[1]} {moving[0]}");
             Sequence s = DOTween.Sequence();
             int tempTarget = target;
@@ -138,15 +105,30 @@ namespace Game.Odometer
             {
                 int targetDigit = tempTarget % 10;
                 tempTarget /= 10;
-
-                // Every wheel rotates once to its new position simultaneously
-                // No wheel will spin more than 180 degrees to get there
-                s.Join(numbers[i].GetRotationTween(targetDigit, duration,moving[i]));
+                var t = numbers[i].GetRotationTween(targetDigit, duration, moving[i]);
+                t.SetDelay(i * 0.25f);
+                s.Join(t);
             }
 
             currentValue = targetValue;
             await s.ToUniTask();
         }
+
+        private void UpdateMovingForAnimation()
+        {
+            for (int i = moving.Length-1; i > 0; i--)
+            {
+                if (moving[i])
+                {
+                    for (int j = i-1; j >= 0; j--)
+                    {
+                        moving[j] = true;
+                    }
+                    break;
+                }
+            }
+        }
+
         public  int SubtractAndUpdateMoving(
             int originalNumber,
             int subtractValue)
@@ -180,7 +162,7 @@ namespace Game.Odometer
                 subtractValue /= 10;
                 index++;
             }
-
+            
             // Clear remaining digits if moving is longer
             for (; index < moving.Length; index++)
                 moving[index] = false;
--- Assets/Game/Odometer/MeterNumber.cs
+++ Assets/Game/Odometer/MeterNumber.cs
@@ -8,7 +8,6 @@ namespace Game.Odometer
     {
         public int CurrentDigit { get; private set; } = -1;
         private const float degreesPerDigit = 36f;
-        private float currentRotationX;
         
         // Helper to set the initial position without animation
         public void SetDigitInstant(int digit)
@@ -19,26 +18,13 @@ namespace Game.Odometer
         
         public Tween GetRotationTween(int digit, float duration,bool alwaysMove=true)
         {
-           // int steps = (10-(digit - CurrentDigit + 10) % 10);
            int steps = (CurrentDigit - digit + 10) % 10;
             if (alwaysMove && steps == 0) steps=10;
             var change = -(steps * degreesPerDigit);
-            currentRotationX += change;
-     
             CurrentDigit = digit;
             return transform.DOLocalRotate(new Vector3(change, 0, 0), duration, RotateMode.LocalAxisAdd)
-                .SetEase(Ease.InOutQuart); // Smooth start and stop for all wheels
+                .SetEase(Ease.InOutQuart); 
         }
         
-        
-        // This is called every frame during a fast spin
-        public void UpdateRotationContinuous(float value)
-        {
-            // If your numbers are 0, 1, 2... in order:
-            float targetAngle = value * degreesPerDigit;
-        
-            // We use localRotation directly for maximum performance during fast updates
-            transform.localRotation = Quaternion.Euler(targetAngle, 0, 0);
-        }
     }
 }
